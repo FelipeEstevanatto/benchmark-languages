2600a520b6c1058306377c20319d7c59bd6d0bcc
Felipe Estevanatto <69355764+FelipeEstevanatto@users.noreply.github.com>
2024-04-18 10:08:59 -0300

    final change

--- average.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Extract execution times and calculate average
-average=$(awk '/Execution time:/ { sum += $3; count++ } count % 3 == 0 { print "Average execution time: " sum/count " ms"; sum = 0; count = 0 }' ./Results/C/BucketSort.txt)
-
-echo "Average execution time: $average ms"
--- graph.py
+++
@@ -1,5 +0,0 @@
-# Take the information from the sorting .txt inside Results, average every 3 values and plot them
-# The x-axis is the number of elements in the array, the y-axis is the time taken to sort the array
-# The plot is saved as a .png file in the Results folder
-
-import matplotlib.pyplot as plt
